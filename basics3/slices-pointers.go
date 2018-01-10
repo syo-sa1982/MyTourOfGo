6f6bfba616802b89826fe6d264d4d14cb388152d
syo-sa1982 <fujii1308@gmail.com>
2018-01-11 07:38:38 +0900

    basics3-9

--- basics3/slices-pointers.go
+++ basics3/slices-pointers.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	fmt.Println("### スライスは配列への参照のようなものです ###")
 	names := [4]string{
 		"John",
 		"Paul",
@@ -18,4 +20,31 @@ func main() {
 	b[0] = "XXX"
 	fmt.Println(a, b)
 	fmt.Println(names)
+
+	// Slice literals
+	/*
+	これは配列リテラル
+	[3]bool{true, true, false}
+	スライス定義するときはこう
+	[]bool{true, true, false}
+	 */
+	fmt.Println("### Slice literals ###")
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
 }
