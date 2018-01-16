1676e600d3942dcf435700ca30379cd69b52db7b
syo-sa1982 <fujii1308@gmail.com>
2018-01-16 13:07:55 +0900

    basic3-21

--- basics3/maps.go
+++ basics3/maps.go
@@ -8,10 +8,26 @@ type Vertex3 struct {
 
 var m map[string]Vertex3
 
+var m3 = map[string]Vertex3{
+	"Bell Labs": {40.68433, -74.39967},
+	"Google": {37.42202, -122.08408},
+}
+
 func main() {
 	m = make(map[string]Vertex3)
 	m["Bell Labs"] = Vertex3{
 		40.68433, -74.39967,
 	}
+
+	var m2 = map[string]Vertex3{
+		"Bell Labs": {
+			40.68433, -74.39967,
+		},
+		"Google": {
+			37.42202, -122.08408,
+		},
+	}
 	fmt.Println(m["Bell Labs"])
+	fmt.Println(m2)
+	fmt.Println(m3)
 }
